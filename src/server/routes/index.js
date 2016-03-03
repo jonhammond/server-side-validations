106dcfde2ccb16c0bfd68d8edc84ebbb440f77c9
bbouley <b.bouley@hotmail.co.uk>
2016-03-03 13:47:06 -0700

    removed route code for wes

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,31 +6,9 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-    var postObj = req.body;
-    var validMessage = validateObject(postObj)
-    console.log(validMessage);
-    if (validMessage === true) {
-        res.render('index', {title: 'Express Form Validation', success : 'All good!'})
-    } else {
-        // console.log(validMessage);
-        res.status(200).render('index', {title: 'Express Form Validation', error : validMessage})
-    }
+    console.log(req.body);
+    res.status(200).send('Yay, everythings GREAT!!!!')
 });
 
 module.exports = router;
 
-
-function validateObject (obj) {
-    var email = obj.email;
-    var option = obj.option;
-    var message = obj.message;
-    if(email.trim() === '' && message.trim() === '') {
-        return ('Message and Email can\'t be blank!!')
-    } else if (email.trim() === '') {
-        return ('Email can\'t be blank!!!')
-    } else if (message.trim() ==='') {
-        return ('Message can\'t be blank')
-    } else {
-        return true;
-    }
-}
