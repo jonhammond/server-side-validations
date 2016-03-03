ca6b10641237148dc2a11239ebe979bbeb589916
bbouley <b.bouley@hotmail.co.uk>
2016-03-03 09:38:20 -0700

    updated with skeleton and form

---
+++ src/server/routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express Form Validation' });
+});
+
+router.post('/', function(req, res, next) {
+    var postObj = req.body;
+    var validMessage = validateObject(postObj)
+    console.log(validMessage);
+    if (validMessage === true) {
+        res.render('index', {title: 'Express Form Validation', success : 'All good!'})
+    } else {
+        // console.log(validMessage);
+        res.render('index', {title: 'Express Form Validation', error : validMessage})
+    }
+});
+
+module.exports = router;
+
+
+function validateObject (obj) {
+    var email = obj.email;
+    var option = obj.option;
+    var message = obj.message;
+    if(email.trim() === '' && message.trim() === '') {
+        return ('Message and Email can\'t be blank!!')
+    } else if (email.trim() === '') {
+        return ('Email can\'t be blank!!!')
+    } else if (message.trim() ==='') {
+        return ('Message can\'t be blank')
+    } else {
+        return true;
+    }
+}
