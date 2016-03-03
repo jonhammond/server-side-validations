a302c1ccac6dcbb98dc8d9e9014c7828739a455a
Jon Hammond <jonh1016@gmail.com>
2016-03-03 14:41:42 -0700

    Validate function has been written and will now render error messages when there is an error. Still haven't done anything with routes to a database yet.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,14 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+var emails = ["jonh1016@gmail.com", "tedjones@dickhead.com"];
+
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express Form Validation' });
 });
 
 router.post('/', function(req, res, next) {
-    console.log(req.body);
-    res.status(200).render('index', { title: 'You done a validate.' })
+    var opt = req.body.option;
+    var email = req.body.email;
+    var errors = validate(req.body);
+
+    if ( emails.indexOf(email) !== -1) {
+      res.render ('index', {
+        title: 'Some Errors:',
+        errors: errors});
+    }
+
+    else if(!opt) {
+      res.render('index', { title: "Some errors", errors: errors });
+    }
+    // console.log(req.body);
+    else {
+      res.status(200).render('index', { title: 'You done a validate.' });
+    }
 });
 
 module.exports = router;
 
+function validate (body) {
+  var errors = [];
+  if ( emails.indexOf(body.email) !== -1) {
+    errors.push('Email already exists!');
+  }
+  if (!body.option) {
+    errors.push('No hate mail please');
+  }
+  return errors;
+}
